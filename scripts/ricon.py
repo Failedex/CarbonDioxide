bddf8177b7e47255b49d28c634250ff9c4832b90
FailedEx <literallyasalmon@gmail.com>
2025-05-12 23:04:36 +1200

    notification highlight

--- scripts/ricon.py
+++ scripts/ricon.py
@@ -6,6 +6,8 @@ import dbus
 import dbus.service
 from gi.repository import GLib
 from threading import Thread
+import subprocess
+import json
 
 FPS = 30
 DX = lambda t: 1 - (1-t)*(1-t)
@@ -20,6 +22,9 @@ class RIcon(dbus.service.Object):
         self.rotate = 0
         self.target = 0
         self.anim_id = 0
+        self.pctlspin = False
+        self.mode = 0
+        self.dr = 1
 
     def update(self): 
         f = self.rotate
@@ -33,7 +38,7 @@ class RIcon(dbus.service.Object):
             if t >= 1: 
                 dx = 1
 
-            if lid != self.anim_id: 
+            if lid != self.anim_id or self.mode != 0: 
                 return
             
             self.rotate = (self.target-f) * dx + f
@@ -46,12 +51,48 @@ class RIcon(dbus.service.Object):
         self.rotate = self.target
         print(self.rotate, flush = True)
 
+    def PctlSpin(self):
+        proc = subprocess.Popen("./scripts/cava_pipe.sh", stdout=subprocess.PIPE, text=True)
+        while self.mode == 1:
+            out = proc.stdout.readline().strip()
+            data = json.loads(out)
+            self.rotate += data[1] * (3/100)
+            self.rotate %= 100
+            print(self.rotate, flush=True)
+        proc.kill()
+
+    def LinSpin(self): 
+        while self.mode == 2:
+            self.rotate += self.dr
+            self.rotate %= 100
+            print(self.rotate, flush=True)
+            time.sleep(1/FPS)
+
     @dbus.service.method("com.Failed.RIcon", in_signature="i", out_signature="")
     def Set(self, r): 
+        if self.mode != 0: 
+            return
         self.target = r
         self.anim_id += 1
         thd = Thread(target=self.update)
         thd.start()
+
+    @dbus.service.method("com.Failed.RIcon", in_signature="i", out_signature="")
+    def SetMode(self, mode): 
+        self.mode = mode
+        if self.mode == 0:
+            self.Set(0)
+        elif self.mode == 1:
+            thd = Thread(target=self.PctlSpin)
+            thd.start()
+        elif self.mode == 2: 
+            thd = Thread(target=self.LinSpin)
+            thd.start()
+
+    @dbus.service.method("com.Failed.RIcon", in_signature="i", out_signature="")
+    def SetLinSpeed(self, r): 
+        self.dr += r
+        self.dr = min(max(self.dr, 0), 100)
 
 if __name__ == "__main__": 
     if len(sys.argv) <= 1: 
@@ -64,12 +105,20 @@ if __name__ == "__main__":
             exit(0)
     else:
         a = sys.argv[1]
-
+        bus = dbus.SessionBus()
+        remote = bus.get_object("com.Failed.RIcon", "/com/Failed/RIcon")
         if a.isdigit():
-            bus = dbus.SessionBus()
-            remote = bus.get_object("com.Failed.RIcon", "/com/Failed/RIcon")
             remote.Set(int(a))
-        else: 
-            bus = dbus.SessionBus()
-            remote = bus.get_object("com.Failed.RIcon", "/com/Failed/RIcon")
-            remote.Set(100 + (len(a) - 1)*5)
+        elif a == 'mode': 
+            b = int(sys.argv[2]) if len(sys.argv) > 2 and sys.argv[2].isdigit() else 0
+            remote.SetMode(b)
+        elif a == 'type': 
+            b = sys.argv[2] if len(sys.argv) > 2 else ""
+            remote.Set(100 + len(b)*5)
+        elif a == 'inc': 
+            b = int(sys.argv[2]) if len(sys.argv) > 2 and sys.argv[2].isdigit() else 0
+            remote.SetLinSpeed(b)
+        elif a == 'dec': 
+            b = int(sys.argv[2]) if len(sys.argv) > 2 and sys.argv[2].isdigit() else 0
+            remote.SetLinSpeed(-b)
+
